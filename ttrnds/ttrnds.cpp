4423eb9cd39028561df8477b8f9562a6284a10f8
kitasola <e1611@s.akashi.ac.jp>
2018-09-17 14:19:42 +0900

    変数名などコード成型作業

--- ttrnds/ttrnds.cpp
+++ ttrnds/ttrnds.cpp
@@ -24,7 +24,7 @@ inline double wheel_Func(double rad);
 
 int main(void) {
   DualShock3 Controller;
-  gpioInitialise();
+  // gpioInitialise();
   MotorSerial ms;
   // MDD通信セットアップ
   try {
@@ -40,62 +40,71 @@ int main(void) {
 
   //---------Time----------
   struct timespec now, prev;
-  long double delta, time = 0;
-
-  //----------IncRotary----------
-  constexpr int Range = 500 * 2;
-  rotaryInc rotary[3] = {rotaryInc(11, 9, true), rotaryInc(27, 17, true), rotaryInc(10, 22, true)};
-  int wheelIn[3] = {0, 0, 0};
-  int wheelInPrev[3] = {0, 0, 0};
+  long double delta, start = 0;
 
   //----------Guess Point----------
   // Origin Point = Centerof Robot Square
   constexpr int firstX = 740, firstY = 1520 + 89;
   constexpr double firstDeg = 0;
-  double nowPoint[3] = {firstX, firstY, 0};
-  double diffXY[2] = {}, diffV, diffR;
-  constexpr double MatrixPoint[3][3] = {{2.0 / 3.0, -1.0 / 3.0, -1.0 / 3.0}, {0, 1.0 / ROOT3, -1.0 / ROOT3}, {-1.0 / 3.0, -1.0 / 3.0, -1.0 / 3.0}};
-  double constexpr WheelCirc = 101.6 * M_PI;
-  
+  double nowPoint[3] = {firstX, firstY, firstDeg};
+  double deltaX, deltaY, deltaL, deltaA;
+  constexpr double MatrixPoint[3][3] = {{2.0 / 3.0, -1.0 / 3.0, -1.0 / 3.0},
+                                        {0, 1.0 / ROOT3, -1.0 / ROOT3},
+                                        {-1.0 / 3.0, -1.0 / 3.0, -1.0 / 3.0}};
+
   // bia UltraSonic
   // constexpr int measureX0 = 400, measureY0 = 300;
   // constexpr double UltraReg = 1.05;
- 
+
   //----------Plan Root----------
-  //Goal
-  constexpr int TwoTableX = 3000, TwoTableY = 3500, TwoTableR = 1000, TwoTableAngle = 20;
+  // Goal
+  constexpr int TwoTableX = 3000, TwoTableY = 3500, TwoTableR = 1000,
+                TwoTableAngle = 20;
   int twoTableDeg = 0;
-  int goalX = TwoTableX + TwoTableR * sin(twoTableDeg), goalY = TwoTableY - TwoTableR * cos(twoTableDeg);
-  // bia Field View, also yawGoal
+  int goalX = firstX, goalY = firstY;
+
+  // bia Field View, also yawGoal = moment
   double velocityF, angleF;
 
   //----------Movement----------
   // OutPut
+  constexpr int WheelID[3] = {1, 2, 3};
   constexpr int SpeedMax = 240;
-  constexpr int SpeedMin = 7; 
-  int wheelOut[3];
+  constexpr int SpeedMin = 7;
   constexpr double wheelDeg[3] = {0, M_PI_3 * 2, -M_PI_3 * 2};
   double wheelSlow;
-  int wheelSpeed[3], wheelGoal[3], wheelDelta[3], wheelPrev[3];
-  // WheelSpeed Control from  Accel
-  // Result:Speed[mm / s], Goal : Goal[mm / s] Control; Out[PWM](define before)
-  // constexpr double wheelProp = 1, wheelInt = 0, wheelDeff = 0;
+  int wheelOut[3];
 
   // Input Robot View
   double velocityR, angleR, moment;
 
+  // Option
+  // WheelSpeed Control from  Accel
+  // Result: velocityR[PWM], Goal: velocityRGoal[PWM], Control: velocityR[PWM]
+
   // Lock Angle bia PID
-  // Result: yaw, Goal: yawLock, Control: moment(define before), [mm/s]
+  // Result: yaw[degree], Goal: yawLock[degree], Control: moment(define
+  // before)[PWM]
   double yaw, yawDelta, yawPrev;
   double yawGoal = firstDeg;
   constexpr double yawProp = 10, yawInt = 0, yawDeff = 0;
-  //constexpr double yawProp = 22.8, yawInt = 62.4, yawDeff = 2.0;
+  // constexpr double yawProp = 22.8, yawInt = 62.4, yawDeff = 2.0;
 
   //----------Calibration----------
   UPDATELOOP(Controller,
              !(Controller.button(RIGHT) && Controller.button(SQUARE))) {}
+  //----------Gyro----------
   GY521 gyro(0x68, 2, 1000, 1.0);
   gyro.start(firstDeg);
+
+  //----------IncRotary----------
+  constexpr int Range = 500 * 2;
+  constexpr double WheelCirc = 101.6 * M_PI;
+  rotaryInc rotary[3] = {rotaryInc(11, 9, true), rotaryInc(27, 17, true),
+                         rotaryInc(10, 22, true)};
+  int wheelIn[3] = {};
+  int wheelInPrev[3] = {};
+  double wheelSpeed[3] = {};
 
   cout << "Main Start" << endl;
   gpioWrite(BCheck, 1);
@@ -105,71 +114,74 @@ int main(void) {
   UPDATELOOP(Controller,
              !(Controller.button(START) && Controller.button(CROSS))) {
     //----------Sensor----------
-    // Reset
-    if (Controller.button(RIGHT) && Controller.button(SQUARE)) {
-      gyro.resetYaw(firstDeg);
-      nowPoint[0] = firstX;
-      nowPoint[1] = firstY;
-      twoTableDeg = 0;
-    }
     // time
     prev = now;
     clock_gettime(CLOCK_REALTIME, &now);
     delta = now.tv_sec - prev.tv_sec +
             (long double)(now.tv_nsec - prev.tv_nsec) / 1000000000;
-    time += delta;
+    start += delta;
 
     // GY521
     gyro.updata();
     yaw = gyro.yaw;
-    
+
     // RotaryInc
     for (int i = 0; i < 3; ++i) {
       wheelInPrev[i] = wheelIn[i];
       wheelIn[i] = rotary[i].get();
       wheelSpeed[i] =
-          (double)(wheelIn[i] - wheelInPrev[i]) / Range * WheelCirc / delta;
+          (double)(wheelIn[i] - wheelInPrev[i]) * WheelCirc / (double)Range;
       cout << wheelIn[i] << ",";
     }
     cout << endl;
 
-    //-----------Guess Field----------
-    diffXY[0] = diffXY[1] = 0;
-    for(int i = 0; i < 2; ++i){
-      for(int j = 0; j < 3; ++j){
-        diffXY[i] += MatrixPoint[i][j] * (double)(wheelIn[j] - wheelInPrev[j]) * WheelCirc / (double)Range;
-      }
+    //----------Reset----------
+    if (Controller.button(RIGHT) && Controller.button(SQUARE)) {
+      gyro.resetYaw(firstDeg);
+      nowPoint[0] = goalX = firstX;
+      nowPoint[1] = goalY = firstY;
+      twoTableDeg = 0;
     }
-    diffV = hypot(diffXY[0], diffXY[1]);
-    diffR = atan2(diffXY[1], diffXY[0]);
-    nowPoint[0] += diffV * cos(diffR + yaw * M_PI / 180);
-    nowPoint[1] += diffV * sin(diffR + yaw * M_PI / 180);
-    cout << nowPoint[0] << ", " << nowPoint[1]<< ", " << yaw << endl;
+
+    //-----------Guess Field----------
+    deltaX = deltaY = 0;
+    for (int i = 0; i < 3; ++i) {
+      deltaX += MatrixPoint[0][i] * wheelSpeed[i];
+      deltaY += MatrixPoint[1][i] * wheelSpeed[i];
+    }
+    deltaL = hypot(deltaX, deltaY);
+    deltaA = atan2(deltaY, deltaX);
+    nowPoint[0] += deltaL * cos(deltaA + yaw * M_PI / 180);
+    nowPoint[1] += deltaL * sin(deltaA + yaw * M_PI / 180);
+    cout << nowPoint[0] << ", " << nowPoint[1] << ", " << yaw << endl;
 
     //----------Plan Root----------
-    if(Controller.press(CIRCLE)){
+    if (Controller.press(CIRCLE)) {
+      goalX = TwoTableX + TwoTableR * sin(yawGoal * M_PI / 180);
+      goalY = TwoTableY - TwoTableR * cos(yawGoal * M_PI / 180);
+      yawGoal = twoTableDeg;
       twoTableDeg = (twoTableDeg + TwoTableAngle) % 360;
     }
-    goalX = TwoTableX + TwoTableR * sin(yawGoal * M_PI / 180);
-    goalY = TwoTableY - TwoTableR * cos(yawGoal * M_PI / 180);
-    yawGoal = twoTableDeg;
 
     velocityF = hypot(goalY - nowPoint[1], goalX - nowPoint[0]);
     angleF = atan2(goalY - nowPoint[1], goalX - nowPoint[0]);
 
     //----------Movement----------
     // Input
+    /*---------- Manual
     int stickX = Controller.stick(LEFT_X);
     int stickY = -Controller.stick(LEFT_Y);
     angleF = atan2(stickY, stickX);
     velocityF = hypot(stickX, stickY) * (fabs(0.58 * cos(2 * angleF)) + 1.4);
     velocityR = velocityF;
-    if(velocityR > SpeedMax){
+    ----------*/
+    velocityR = velocityF;
+    if (velocityR > SpeedMax) {
       velocityR = SpeedMax;
     }
     angleR = angleF - yaw * M_PI / 180;
 
-    /*
+    /*----------Manual
     moment = -(Controller.stick(LEFT_T) - Controller.stick(RIGHT_T)) * 0.5;
     if(moment == 0){
       yawPrev = yawDelta;
@@ -183,7 +195,7 @@ int main(void) {
               yawDeff * (yawDelta - yawPrev) / delta;
       moment *= -1;
     }
-    */
+    ----------*/
     // moment frome Lock Angle bia PID
     yawPrev = yawDelta;
     yawDelta = yawGoal - yaw;
@@ -193,7 +205,7 @@ int main(void) {
       yawDelta += 360;
     }
     moment = yawProp * yawDelta + yawInt * yawDelta * delta +
-            yawDeff * (yawDelta - yawPrev) / delta;
+             yawDeff * (yawDelta - yawPrev) / delta;
     moment *= -1;
 
     if (moment > 125) {
@@ -202,12 +214,12 @@ int main(void) {
       moment = -125;
     }
 
-    // wheelGoal
+    // wheelOut
     int dummyMax = SpeedMax;
     for (int i = 0; i < 3; ++i) {
-      wheelGoal[i] = velocityR * wheel_Func(angleR + wheelDeg[i]) + moment;
-      if (abs(wheelGoal[i]) > dummyMax) {
-        dummyMax = abs(wheelGoal[i]);
+      wheelOut[i] = velocityR * wheel_Func(angleR + wheelDeg[i]) + moment;
+      if (abs(wheelOut[i]) > dummyMax) {
+        dummyMax = abs(wheelOut[i]);
       }
     }
     wheelSlow = SpeedMax / (double)dummyMax;
@@ -218,30 +230,12 @@ int main(void) {
     }
 
     for (int i = 0; i < 3; ++i) {
-      wheelGoal[i] *= wheelSlow;
-      if (0 < wheelGoal[i] && wheelGoal[i] < SpeedMin) {
-        wheelGoal[i] = 0;
-      } else if (0 > wheelGoal[i] && wheelGoal[i] > -SpeedMin) {
-        wheelGoal[i] = 0;
+      wheelOut[i] *= wheelSlow;
+      if (0 < wheelOut[i] && wheelOut[i] < SpeedMin) {
+        wheelOut[i] = 0;
+      } else if (0 > wheelOut[i] && wheelOut[i] > -SpeedMin) {
+        wheelOut[i] = 0;
       }
-    }
-
-    // 2018/7/19 without PID
-    // wheelOut & PID
-    /*
-    for (int i = 0; i < 3; ++i) {
-      wheelGoal[i] *= wheelSlow;
-      wheelPrev[i] = wheelDelta[i];
-      wheelSpeed[i] = (wheelIn[i] - wheelInPrev[i]) / Range * WheelCirc / delta;
-      wheelDelta[i] = wheelGoal[i] - wheelSpeed[i];
-      wheelOut[i] = wheelProp * wheelDelta[i] +
-                    wheelInt * wheelDelta[i] * delta +
-                    wheelDeff * (wheelDelta[i] - wheelPrev[i]) / delta;
-    }
-    */
-    // wheelGoal change wheelOut
-    for (int i = 0; i < 3; ++i) {
-      wheelOut[i] = wheelGoal[i];
     }
 
     /*
@@ -252,7 +246,7 @@ int main(void) {
     */
 
     for (int i = 0; i < 3; ++i) {
-      ms.send(i + 1, 2, wheelOut[i]);
+      ms.send(WheelID[i], 2, wheelOut[i]);
     }
 
     //----------Emergency----------
