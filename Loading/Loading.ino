16f22fc9b4f0f1eefa3c2640264c667a6b5cd757
Kitasola <e1611@s.akashi.ac.jp>
2018-09-27 08:08:43 +0900

    装填改良

--- Loading/Loading.ino
+++ Loading/Loading.ino
@@ -35,13 +35,13 @@ void setup() {
 }
 
 constexpr int Spin = 30;
-constexpr int DelaySolenoid = 250, DelayLoad = 500, DelayArm = 1000, DelayHand = 500;
+constexpr int DelaySolenoid = 250, DelayLoad = 500, DelayArm = 1000, DelayHand = 550;
 int delayShoot = 310;
 constexpr int LimitFall = A2, LimitCatch = A3, LimitArm = A0, Solenoid[2] = {10, 11}, Arm = 3, Hand = 9;
 unsigned long now = millis();
 unsigned long prevUp = now, prevDown = now, prevArm = now, prevHand = now, prevShoot = now;
 boolean loadable = false;
-boolean flagFB= false, flagHand = false;
+boolean flagFB = false, flagHand = false;
 boolean shootable = true, order = false;
 boolean nowCatch = false, prevCatch = false;
 int phase = 6;
@@ -51,70 +51,70 @@ void loop() {
   now = millis();
 
   // Act
-  switch(phase){
+  switch (phase) {
     case 0:
-    driveMtr(Spin);
-    break;
-    
+      driveMtr(Spin);
+      break;
+
     case 1:
-    driveMtr(0);
-    digitalWrite(Arm, 0);
-    break;
-    
+      driveMtr(0);
+      digitalWrite(Arm, 0);
+      break;
+
     case 2:
-    driveMtr(Spin);
-    break;
-    
+      driveMtr(Spin);
+      break;
+
     case 3:
-    driveMtr(0);
-    digitalWrite(Hand, 0);
-    break;
-    
+      driveMtr(0);
+      digitalWrite(Hand, 0);
+      break;
+
     case 4:
-    driveMtr(Spin);
-    break;
-    
+      driveMtr(Spin);
+      break;
+
     case 5:
-    driveMtr(0);
-    digitalWrite(Solenoid[flagFB], 1);
-    break;
-    
+      driveMtr(0);
+      digitalWrite(Solenoid[flagFB], 1);
+      break;
+
     case 6:
-    digitalWrite(Solenoid[flagFB], 0);
-    break;
+      digitalWrite(Solenoid[flagFB], 0);
+      break;
   }
 
   // Judge
-  if(phase == 0 && digitalRead(LimitArm)){
+  if (phase == 0 && digitalRead(LimitArm)) {
     prevArm = now;
     phase = 1;
   }
-  else if(phase == 2 && digitalRead(LimitCatch)){
+  else if (phase == 2 && digitalRead(LimitCatch)) {
     prevHand = now;
     phase = 3;
   }
-  else if(phase == 1 && now - prevArm > DelayArm){
+  else if (phase == 1 && now - prevArm > DelayArm) {
     phase = 2;
   }
-  else if(phase == 3 && now - prevHand > DelayHand){
+  else if (phase == 3 && now - prevHand > DelayHand) {
     phase = 4;
   }
-  else if(phase == 4 && digitalRead(LimitFall)){
+  else if (phase == 4 && digitalRead(LimitFall)) {
     prevUp = now;
     loadable = false;
     shootable = true;
     phase = 5;
   }
-  else if(phase == 5 && now - prevUp > DelaySolenoid){
+  else if (phase == 5 && now - prevUp > DelaySolenoid) {
     prevDown = now;
     phase = 6;
   }
-  else if(phase == 6 && loadable && now - prevDown > DelayLoad){
+  else if (phase == 6 && loadable && now - prevDown > DelayLoad) {
     phase = 0;
     flagFB = !flagFB;
   }
 
-  if(order){
+  if (order) {
     digitalWrite(Arm, 1);
     shootable = false;
     loadable = true;
@@ -122,22 +122,21 @@ void loop() {
     prevShoot = now;
     flagHand = true;
   }
-  else if(now - prevShoot > delayShoot && flagHand){
+  else if (now - prevShoot > delayShoot && flagHand) {
     digitalWrite(Hand, 1);
     flagHand = false;
   }
-  Serial.print(phase);
 }
 
-boolean checker(int rx_data, int& tx_data){
-  if(rx_data != 0){
+boolean checker(int rx_data, int& tx_data) {
+  if (rx_data != 0) {
     order = shootable;
   }
   else {
     order = false;
   }
-  if(order){
-    delayShoot = rx_data * 10;
+  if (order) {
+    delayShoot = rx_data * 5;
   }
   return shootable;
 }
