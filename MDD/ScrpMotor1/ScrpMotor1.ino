9c8a08e55782891910e78f03d1cb1ab39a5dbd4b
Kitasola <e1611@s.akashi.ac.jp>
2018-10-12 23:32:32 +0900

    TapeLED追加

--- MDD/ScrpMotor1/ScrpMotor1.ino
+++ MDD/ScrpMotor1/ScrpMotor1.ino
@@ -1,3 +1,4 @@
+#include <Adafruit_NeoPixel.h>
 #include <ScrpSlave.h>
 #include <Utility.h>
 #include <EEPROM.h>
@@ -15,6 +16,23 @@ constexpr int Motor[3][3] = {
 };
 constexpr int BaudRate = 115200, RedePin = 4;
 
+// Tape LED
+constexpr int Tape[2] = {10, 11};
+constexpr int LEDs = 15;
+Adafruit_NeoPixel Pixels[2] = {
+  Adafruit_NeoPixel(LEDs, 10, NEO_GRB + NEO_KHZ800), // Left
+  Adafruit_NeoPixel(LEDs, 11, NEO_GRB + NEO_KHZ800)  // Right
+};
+uint32_t colors[2][2] = {
+  {Pixels[0].Color(0xFF, 0, 0), Pixels[0].Color(0xFF, 0x69, 0xB4)}, // 2.4 & Other in RedZone
+  {Pixels[0].Color(0, 0, 0xFF), Pixels[0].Color(0x7F, 0xFF, 0xD4)}  // 2.4 & Other in BlueZone
+};
+
+boolean zone, arm, table, doing;
+int stage = 0, interval = 500, stepLED = 0;
+
+unsigned int stageEnd = 0;
+
 void setup() {
   for (int i = 0; i < 3; ++i) {
     for (int j = 0; j < 2; ++j) {
@@ -28,10 +46,16 @@ void setup() {
   slave.addCMD(3, driveMtr2);
   slave.addCMD(4, driveMtr3);
   slave.addCMD(20, ultraDist);
+  slave.addCMD(30, changeLEDFlag);
   slave.addCMD(255, safeOperation);
 
   pinMode(URTRIG, OUTPUT);
   digitalWrite(URTRIG, HIGH);
+
+  Pixels[0].begin();
+  Pixels[1].begin();
+  Pixels[0].show(); // Set all LED to OFF
+  Pixels[1].show();
 }
 
 int dist = 0;
@@ -48,6 +72,15 @@ void loop() {
       dist = sensorValue * 0.718;
     }
     prev_time = millis();
+  }
+
+  if (doing) {
+    if (stage < 1) {
+      onLED();
+    }
+    if (stage < 2 && millis() - stageEnd >= 5000) {
+      offLED(0);
+    }
   }
 }
 
@@ -94,5 +127,54 @@ boolean driveMtr3(int rx_data, int& tx_data) {
 boolean ultraDist(int rx_dara, int& tx_data) {
   tx_data = dist;
   return true;
+}
+
+boolean changeLEDInterval(int rx_data, int& tx_data) {
+  // change LEDwipe interval(ms)
+  interval = rx_data / LEDs;
+  return true;
+}
+
+void onLED() {
+  static unsigned long preLED = 0;
+  static int stepLED = 0;
+  if (millis() - preLED >= interval) {
+    Pixels[arm].setPixelColor(stepLED, colors[zone][table]);
+    Pixels[arm].show();
+    preLED = millis();
+    stepLED++;
+  }
+  if (stepLED >= LEDs) {
+    stageEnd = millis();
+    stage++;
+    preLED = 0;
+    stepLED = 0;
+  }
+}
+
+void offLED(int pattern){
+  static unsigned long preLED = 0;
+  static int stepLED = 0;
+  if (pattern == 0) { // All off
+    for (int i = 0; i < LEDs; ++i){
+      Pixels[arm].setPixelColor(i, Pixels[arm].Color(0, 0, 0));
+    }
+    Pixels[arm].show();
+    doing = 0;
+    stage = 0;
+  }else{ // Wipe off
+    if (millis() - preLED >= interval) {
+      Pixels[arm].setPixelColor(stepLED, Pixels[arm].Color(0, 0, 0));
+      Pixels[arm].show();
+      preLED = millis();
+      stepLED++;
+    }
+    if (stepLED >= LEDs) {
+      preLED = 0;
+      stepLED = 0;
+      doing = 0;
+      stage = 0;
+    }
+  }
 }
 
